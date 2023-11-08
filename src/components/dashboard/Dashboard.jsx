77ef926c71d633571e06d6fadc68b1a0d06d3155
Gamenight2008 <u65_tompie_waw@technischools.com>
2023-11-08 18:54:03 +0100

    Skończona całość, działa już idealnie

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,104 +1,124 @@
-import './Dashboard.scss';
-import { useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
-import { AppContext } from '../../context/app.context';
+import "./Dashboard.scss";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AppContext } from "../../context/app.context";
 
 const Dashboard = () => {
-   const {
-       setLink,
-       boxColor,
-       setBoxColor,
-       currentOrder,
-       setOrder,
-       footerText,
-       setFooter
-   } = useContext(AppContext);
+  const {
+    setLink,
+    boxColor,
+    setBoxColor,
+    currentOrder,
+    setOrder,
+    footerText,
+    setFooter,
+  } = useContext(AppContext);
 
-   const navigate = useNavigate();
+  const navigate = useNavigate();
 
-   const setFooterText = (e) => {
-       setFooter(prev => ({ ...prev, [e.target.name]: e.target.value }));
-       localStorage.setItem('footerText', JSON.stringify(e.target.value));
-   }
+  const setFooterText = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFooter((prev) => {
+      const newFooter = { ...prev, [name]: value };
+      localStorage.setItem("footerText", JSON.stringify(newFooter));
+      return newFooter;
+    });
+  };
 
-   const setImage = (e) => {
-       setLink(prev => ({ ...prev, [e.target.name]: e.target.value }));
-       localStorage.setItem('link', JSON.stringify(e.target.value));
-   }
+  const setImage = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setLink((prev) => {
+      const newLink = { ...prev, [name]: value };
+      localStorage.setItem("link", JSON.stringify(newLink));
+      return newLink;
+    });
+  };
 
-   const handleColorChange = (e) => {
-       setBoxColor(prev => ({ ...prev, [e.target.name]: e.target.value }));
-       localStorage.setItem('boxColor', JSON.stringify(e.target.value));
-   }
+  const handleColorChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setBoxColor((prev) => {
+      const newBoxColor = { ...prev, [name]: value };
+      localStorage.setItem("boxColor", JSON.stringify(newBoxColor));
+      return newBoxColor;
+    });
+  };
 
-   const handleOrderChange = (e) => {
-       setOrder(prev => ({ ...prev, [e.target.name]: parseInt(e.target.value, 10) }));
-       localStorage.setItem('currentOrder', JSON.stringify(e.target.value));
-   }
+  const handleOrderChange = (e) => {
+    const name = e.target.name;
+    const value = parseInt(e.target.value, 10);
+    setOrder((prev) => {
+      const newOrder = { ...prev, [name]: value };
+      localStorage.setItem("currentOrder", JSON.stringify(newOrder));
+      return newOrder;
+    });
+  };
 
-   const LogOut = () => {
-       navigate("/", { replace: true });
-   }
+  const LogOut = () => {
+    navigate("/", { replace: true });
+  };
 
-   return (
-       <div className="dashboard-container">
-           <div className="section-settings">
-               <div className="title">
-                   <h2>Slider</h2>
-                   <button>ON/OFF</button>
-               </div>
-               <div className="settings">
-                   <button onClick={LogOut}>Log out</button>
+  return (
+    <div className="dashboard-container">
+      <div className="section-settings">
+        <div className="title">
+          <h2>Slider</h2>
+          <button>ON/OFF</button>
+        </div>
+        <div className="settings">
+          <button onClick={LogOut}>Log out</button>
 
-                   {[1, 2, 3].map(num => (
-                       <input
-                           key={num}
-                           name={num.toString()}
-                           onChange={setImage}
-                           type='text'
-                           placeholder={`${num}st Link`}
-                       />
-                   ))}
+          {[1, 2, 3].map((num) => (
+            <input
+              key={num}
+              name={num.toString()}
+              onChange={setImage}
+              type="text"
+              placeholder={`${num}st Link`}
+            />
+          ))}
 
-                   <div className="color-picker">
-                       {["Box one", "Box two", "Box three"].map((label, index) => (
-                           <div key={index}>
-                               <h4>{label}</h4>
-                               <input
-                                   type="color"
-                                   name={(index + 1).toString()}
-                                   className="colorPicker"
-                                   value={boxColor[index + 1]}
-                                   onChange={handleColorChange}
-                               />
-                           </div>
-                       ))}
+          <div className="color-picker">
+            {["Box one", "Box two", "Box three"].map((label, index) => (
+              <div key={index}>
+                <h4>{label}</h4>
+                <input
+                  type="color"
+                  name={(index + 1).toString()}
+                  className="colorPicker"
+                  value={boxColor[index + 1]}
+                  onChange={handleColorChange}
+                />
+              </div>
+            ))}
 
-                       <h4>Order Changer</h4>
-                       {[1, 2, 3].map(num => (
-                           <input
-                               key={num}
-                               type='number'
-                               name={num.toString()}
-                               className='orderPicker'
-                               value={currentOrder[num]}
-                               onChange={handleOrderChange}
-                           />
-                       ))}
+            <h4>Order Changer</h4>
+            {[1, 2, 3].map((num) => (
+              <input
+                key={num}
+                type="number"
+                name={num.toString()}
+                className="orderPicker"
+                value={currentOrder[num]}
+                onChange={handleOrderChange}
+              />
+            ))}
 
-                       <h4>Footer text changer</h4>
-                       <input
-                           type='text'
-                           name="1"
-                           className='picker'
-                           value={footerText[1]}
-                           onChange={setFooterText}
-                       />
-                   </div>
-               </div>
-           </div>
-       </div>
-   );
-}
+            <h4>Footer text changer</h4>
+            <input
+              type="text"
+              name="1"
+              className="picker"
+              value={footerText[1]}
+              onChange={setFooterText}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
